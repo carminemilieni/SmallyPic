6db6bd66ef25445ac2dac9f6369a0ccd3f087efb
Carmine Milieni <carmine.milieni@gmail.com>
2020-01-29 16:20:54 +0100

    up

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
 import * as prettyBytes from 'pretty-bytes';
+import { dialog, BrowserWindow, BrowserWindowConstructorOptions, OpenDialogOptions, MessageBoxSyncOptions } from 'electron';
+import { ElectronService } from '../core/services';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -19,30 +21,43 @@ export class HomeComponent implements OnInit {
     modifiedSize: '0'
   }
 
-  constructor() { }
+  constructor(private electronService: ElectronService) { }
 
   ngOnInit(): void {
     //this.dragzoneInit();
+
   }
 
 
   public onDrop($event: DragEvent) {
+
+    const opt: MessageBoxSyncOptions = {
+      type: 'info',
+      buttons: ['OK'],
+      title: 'File non compatibile',
+      message: 'File non compatibile'
+    };
+    console.log(this.electronService.remote.dialog.showMessageBoxSync(null, opt));
+
     console.log('ciao')
     const files = $event.dataTransfer.files;
-    const file = files[0];
+    const file: File = files[0];
 
-    console.log(file);
 
     this.file.originalSize = prettyBytes(file.size);
 
     console.log(prettyBytes(file.size));
 
-    if (file.type !== 'image/jpeg' || file.type !== 'image/png') {
+    if (file.type.toString() !== 'image/jpeg' || file.type.toString() !== 'image/png' || file.type.toString() !== 'image/jpg') {
+      // dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] });
+      window.alert('estensione errata')
 
     }
   }
 
   public onDragover($event: DragEvent) {
+
+
     $event.preventDefault();
     if (!this.dragzone.dragover && !this.dragzone.drop) {
       this.dragzone.dragover = true;
